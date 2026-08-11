88eb0f3a3de825f6e8b2935df792c6f8848d4716
nnn669 <510875510@qq.com>
2026-08-11 23:08:54 +0800

    chore: bump version to 0.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.agentt.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
