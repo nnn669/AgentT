1ddbdbb575969a96e9a4a881d05af7327c1d7833
nnn669 <510875510@qq.com>
2026-08-11 19:36:14 +0800

    chore: bump version to 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.agentt.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
